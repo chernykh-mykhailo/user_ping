cc21e7a7e1f2bd556fa5eddcae6547e8342836f2
Mykhailo <270775mik@gmail.com>
2025-12-25 16:43:28 +0100

    fix config for deploy

--- config.py
+++ config.py
@@ -13,22 +13,10 @@ BOT_MODE = os.getenv('BOT_MODE', 'test').lower()
 
 if BOT_MODE == 'prod':
     print("🚀 Запуск у ПРОДАКШН режимі")
-    from config_prod import (
-        API_ID, API_HASH, SESSION_NAME, BOT_TOKEN,
-        PAYMENT_TOKEN, ADMIN_USER_ID, DB_FILE, PING_LIMITS,
-        EMOJIS, PREMIUM_PLANS, CHAT_PREMIUM_PLANS,
-        GIFT_PLANS, REFERRAL_BONUS_SIGNUP, REFERRAL_BONUS_PREMIUM,
-        GIFT_DISCOUNT, PROJECTS_CHANNEL, UB_ACCOUNTS
-    )
+    from config_prod import *
 else:
     print("🧪 Запуск у ТЕСТОВОМУ режимі")
-    from config_test import (
-        API_ID, API_HASH, SESSION_NAME, BOT_TOKEN,
-        PAYMENT_TOKEN, ADMIN_USER_ID, DB_FILE, PING_LIMITS,
-        EMOJIS, PREMIUM_PLANS, CHAT_PREMIUM_PLANS,
-        GIFT_PLANS, REFERRAL_BONUS_SIGNUP, REFERRAL_BONUS_PREMIUM,
-        GIFT_DISCOUNT, PROJECTS_CHANNEL, UB_ACCOUNTS
-    )
+    from config_test import *
 
 print(f"📱 Bot Token: {BOT_TOKEN[:20]}...")
 print(f"💾 Database: {DB_FILE}")
